0d990058ee7eb81b1dd74fc37aa8185e1483d8b0
Bishal Poudel <poudelb172@gmail.com>
2022-05-27 15:54:40 +0545

    mysql connection successful

--- myp1/myp1/settings.py
+++ myp1/myp1/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'myp1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecom',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'password'
     }
 }
 
