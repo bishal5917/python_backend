b8e05af465f6178b7bbae58f815ce686e26764d2
Bishal Poudel <poudelb172@gmail.com>
2022-05-30 13:27:33 +0545

    filtering data

--- myp1/home/views.py
+++ myp1/home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from store.models import Product
+from django.db.models import Q
+from store.models import Customer
 
 # Create your views here.
 def sayHello(request):
-    #pk=primary key (django will automatically know it as primary key)
-    product=Product.objects.get()
-    return render(request,'first.html')
+    # pk=primary key (django will automatically know it as primary key)
+    # customer = Customer.objects.filter(pk=1)
+    
+    #using the Q object (for id<2 or first_name="Boka")
+    customer = Customer.objects.filter(Q(id__lt=2) | Q(first_name="Boka"))
+    return render(request, 'first.html', {'cust': customer})
