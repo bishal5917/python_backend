83f99a1f4a56f0919df19720776f5a55eab3c716
Bishal Poudel <poudelb172@gmail.com>
2022-05-30 23:17:01 +0545

    about django ORM

--- myp1/home/views.py
+++ myp1/home/views.py
@@ -3,10 +3,22 @@ from django.db.models import Q
 from store.models import Customer
 
 # Create your views here.
+
+
 def sayHello(request):
     # pk=primary key (django will automatically know it as primary key)
     # customer = Customer.objects.filter(pk=1)
-    
-    #using the Q object (for id<2 or first_name="Boka")
-    customer = Customer.objects.filter(Q(id__lt=2) | Q(first_name="Boka"))
+
+    # using the Q object (for id<2 or first_name="Boka")
+    # customer = Customer.objects.filter(Q(id__lt=2) | Q(first_name="Boka"))
+
+    # order by
+    # customer = Customer.objects.order_by('first_name')
+
+    # limiting results
+    # customer = Customer.objects.all()[:2]
+
+    # writing raw queries
+    customer = Customer.objects.raw("SELECT * FROM store_customers where id=2")
+
     return render(request, 'first.html', {'cust': customer})
