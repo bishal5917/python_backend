404a59efd4b5ddbe6896c29d74dddaf31f419cf7
Bishal Poudel <poudelb172@gmail.com>
2022-06-01 20:06:40 +0545

    adding the listings

--- myp1/store/admin.py
+++ myp1/store/admin.py
@@ -3,4 +3,14 @@ from . import models
 
 # Register your models here.
 admin.site.register(models.Collection)
-admin.site.register(models.Customer)
+# admin.site.register(models.Customer)
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 5
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','placed_at','customer']
